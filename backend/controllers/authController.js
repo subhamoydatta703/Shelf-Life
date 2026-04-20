12bb662be91306bba04a6e233498a7684ff48993
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-04-20 22:12:13 +0530

    refactor: improve error handling and validation in authentication controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,52 +1,51 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Joi = require('joi');
+const Joi = require("joi");
 const user = require("../models/user");
 
 const registerSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    confirmPassword: Joi.any().valid(Joi.ref('password')).required().messages({
-        'any.only': 'Passwords do not match'
-    })
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  confirmPassword: Joi.any().valid(Joi.ref("password")).required().messages({
+    "any.only": "Passwords do not match",
+  }),
 });
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
 });
 // register
-const register = async (req, res) => {
+exports.register = async (req, res) => {
+  try {
+    const { error } = registerSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Check your inputs",
+      });
+    }
+    const { name, email, password } = req.body;
 
-    try {
-        const { error } = registerSchema.validate(req.body);
-        if (error) {
-            return res.status(400).json({
-                success: false,
-                message: "Check your inputs",
-            });
-        }
-        const {name, email, password} = req.body;
+    const user = await User.findOne({
+      email: email,
+    });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
 
-        const user = await User.findOne({
-            email: email
-        })
-        if(user){
-            return res.status(400).json({
-                success: false,
-                message: "User already exists",
-            });
-        }
-
-         const hashedPass = await bcrypt.hash(password, 10);
-         const newUser = await User.create({
-            name: name,
-            email: email,
-            password: hashedPass,
-         })
-          let jwtToken = await jwt.sign(
+    const hashedPass = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: hashedPass,
+    });
+    let jwtToken = await jwt.sign(
       {
         userId: newUser._id,
       },
@@ -59,16 +58,62 @@ const register = async (req, res) => {
       token: jwtToken,
       user: newUser,
     });
+  } catch (error) {
+    console.log("Error: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+// login
 
-
-    } catch (error) {
-        console.log("Error: ",error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
-        
+exports.login = async (req, res) => {
+  try {
+    const { error } = loginSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: "Check your inputs",
+      });
     }
-    // login
 
-}
+    const { email, password } = req.body;
+
+    const existUser = await User.findOne({ email: email });
+    if (!existUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User doesn't exists",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, existUser.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    let jwtToken = await jwt.sign(
+      {
+        userId: existUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+    delete existUser.password;
+    res.status(201).json({
+      success: true,
+      message: "User logged in successfully",
+      token: jwtToken,
+      user: existUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
