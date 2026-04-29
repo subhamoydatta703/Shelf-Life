49914c5de55b1125ae40fbb7e16116782902905c
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-04-30 00:58:34 +0530

    feat: update API base URL to use import.meta.env and add vercel configuration file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL+'/api',
+  baseURL: `${import.meta.env.REACT_APP_SERVER_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
